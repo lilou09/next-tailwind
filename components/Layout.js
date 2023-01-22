1700f4b2e1fc22d1f524fb914b41c4fe7c2246b2
lilou09 <loverdo123@gmail.com>
2023-01-22 16:37:15 +0100

    Adding cart.js

---
+++ components/Layout.js
@@ -0,0 +1,57 @@
+import React, { useContext, useEffect, useState } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import { Store } from '@/utils/Store';
+
+const Layout = ({ title, children }) => {
+  const { state } = useContext(Store);
+  const { cart } = state;
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+
+  useEffect(() => {
+    setCartItemsCount(
+      cart.cartItems.reduce((acc, item) => acc + item.quantity, 0)
+    );
+  }, [cart.cartItems]);
+
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - Moshop' : 'Moshop'}</title>
+        <meta name="description" content="Ecommerce website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/" legacyBehavior>
+              <a className="text-lg font-bold">MOSHOP</a>
+            </Link>
+            <div>
+              <Link href="/cart" legacyBehavior>
+                <a className="p-2">
+                  Cart
+                  {cartItemsCount > 0 && (
+                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                      {cartItemsCount}
+                    </span>
+                  )}
+                </a>
+              </Link>
+              <Link href="/login" legacyBehavior>
+                <a className="p-2">Login</a>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner ">
+          <p>Copyright © 2023 MOSHOP</p>
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
